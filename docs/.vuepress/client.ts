05bd35cf6816d13fa08ffe0a6835a6af6f6b9699
salt235 <734489881@qq.com>
2025-12-26 19:46:04 +0800

    修改了字体

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -6,7 +6,7 @@ import { defineClientConfig } from 'vuepress/client'
 
 // import CustomComponent from './theme/components/Custom.vue'
 
-// import './theme/styles/custom.css'
+import './theme/styles/custom.css' // your custom styles
 
 export default defineClientConfig({
   enhance({ app }) {
